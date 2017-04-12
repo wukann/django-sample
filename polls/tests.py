bb7b73a7ef37e2768b01dd942f9abf6b0ff1adc9
wukann <wukann@gmail.com>
2017-04-12 13:55:04 +0900

    IndexViewテスト完了

--- polls/tests.py
+++ polls/tests.py
@@ -65,11 +65,38 @@ class QuestionViewTests(TestCase):
         create_question('Past question.', -30)
         response = self.client.get(reverse('polls:index'))
         self.assertContains(response, 'Past question.')
-        self.assertQuerysetEqual(response.context['latest_question_list'], ['<Question: Past question.>'])
+        self.assertQuerysetEqual(response.context['latest_question_list'], [
+                                 '<Question: Past question.>'])
 
     def test_index_view_with_a_feature_question(self):
         """
         Questions with a pub_date in the future should not be displayed on
         the index page.
         """
-        pass
+        create_question('Future question.', 30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertContains(response, 'No polls are available.')
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_index_view_with_future_question_and_past_question(self):
+        """
+        Even if both past and future questions exist, only past questions
+        should be displayed.
+        """
+        create_question('Past question.', -30)
+        create_question('Future question.', 30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(response.context['latest_question_list'], [
+                                 '<Question: Past question.>'])
+
+    def test_index_view_with_two_past_questions(self):
+        """
+        The questions index page may display multiple questions.
+        """
+        create_question(question_text="Past question 1.", days=-30)
+        create_question(question_text="Past question 2.", days=-5)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question 2.>', '<Question: Past question 1.>']
+        )
